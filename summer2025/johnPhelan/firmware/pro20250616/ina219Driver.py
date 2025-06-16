7ca0916515e094056f2761e5098035002e03f0c5
john phelan <johnlphelan2004@gmail.com>
2025-06-16 17:08:04 -0500

    updated ina219 firmware

--- summer2025/johnPhelan/firmware/pro20250616/ina219Driver.py
+++ summer2025/johnPhelan/firmware/pro20250616/ina219Driver.py
@@ -6,13 +6,13 @@ SHUNT_OHMS = 0.1
 MAX_EXPECTED_AMPS = 0.2
 
 class INA219:
-    def __init__(self, port=5, address=0x40):
-        self.port = port
+    def __init__(self, busnum=5, address=0x40):
+        self.busnum = busnum
         self.address = address
         self.data = None
 
-    def read():
-        ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS, 5)
+    def read(self):
+        ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS, self.busnum)
         ina.configure(ina.RANGE_16V)
 
         print("Bus Voltage: %.3f V" % ina.voltage())
--- summer2025/johnPhelan/firmware/pro20250616/ina219Main.py
+++ summer2025/johnPhelan/firmware/pro20250616/ina219Main.py
@@ -1,8 +1,11 @@
 from ina219Driver import INA219
 import time
 
-ina219 = INA219()
+SHUNT_OHMS = 0.1
+MAX_EXPECTED_AMPS = 0.2
+
+ina = INA219(SHUNT_OHMS, MAX_EXPECTED_AMPS, address=0x40, busnum=5)
 
 while True:
-    ina219.read()
+    ina.read()
     time.sleep(1)

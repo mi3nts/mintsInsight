820c04f1497d09ff04f0962b3349fdfeb4a362a3
john phelan <johnlphelan2004@gmail.com>
2025-06-16 17:37:01 -0500

    working INA219 main for both sensors

--- summer2025/johnPhelan/firmware/pro20250616/ina219Main.py
+++ summer2025/johnPhelan/firmware/pro20250616/ina219Main.py
@@ -1,8 +1,12 @@
 from ina219Driver import INA219DRIVER
 import time
 
-ina = INA219DRIVER(busnum=5, address=0x40)
+ina1 = INA219DRIVER(busnum=5, address=0x40)
+ina2 = INA219DRIVER(busnum=5, address=0x41)
 
 while True:
-    ina.read()
+    print("INA1")
+    ina1.read()
+    print("INA2")
+    ina2.read()
     time.sleep(1)

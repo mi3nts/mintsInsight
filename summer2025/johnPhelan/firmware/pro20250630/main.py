287d972a3b2c950ae7921752505e3683b68123ba
jlp004 <johnlphelan2004@gmail.com>
2025-07-08 12:03:42 -0500

    more clarity in main.py

--- summer2025/johnPhelan/firmware/pro20250630/main.py
+++ summer2025/johnPhelan/firmware/pro20250630/main.py
@@ -16,8 +16,8 @@ while True:
         currentUnfilteredCO2 = sensor.readCO2(with_filter=False) #retry on None until reading found
     dequeCO2.append(currentUnfilteredCO2)
 
-    print("CO₂:", currentFilteredCO2, "ppm")
-    print("uCO2: ", currentUnfilteredCO2, "ppm")
+    print("filtered CO₂:", currentFilteredCO2, "ppm")
+    print("unfiltered CO2: ", currentUnfilteredCO2, "ppm")
     print("CO2 deque: ", dequeCO2)
     print("Temperature:", currentTemp, "°C")
     print("Humidity:", currentHumidity, "%")
